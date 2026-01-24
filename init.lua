075de8cebacd4a0cf7d66902c8939233c11d893a
Sheriff U3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-01-24 13:21:12 -0600

    changed file loading in init.lua

--- init.lua
+++ init.lua
@@ -1,30 +1,24 @@
 circuits = {}
 
---[[ Request insecure environment for luajit bitops
-local insecure = core.request_insecure_environment()
-if not insecure then
-	error(table.concat{"\nCircuits requires bitops library to function.\n"
-			, "Please add circuits to the trusted mods list\n"
-			, "in settings or disable mod security."})
-end
-bit = insecure.require("bit")
-]]
-
 circuits.hash_pos = core.hash_node_position
 circuits.unhash_pos = core.get_position_from_hash
 
-local modpath = core.get_modpath("circuits")
+local function modpath(file)
+	local mod = core.get_current_modname()
+	local modpath = core.get_modpath(mod)
+	return dofile(modpath..file)
+end
 
-dofile(modpath .. "/util.lua")
-dofile(modpath .. "/position.lua")
-dofile(modpath .. "/connection.lua")
-dofile(modpath .. "/persistance.lua")
-dofile(modpath .. "/power.lua")
-dofile(modpath .. "/wire.lua")
-dofile(modpath .. "/wire_node.lua")
-dofile(modpath .. "/lamp.lua")
-dofile(modpath .. "/indicator.lua")
-dofile(modpath .. "/inverter.lua")
-dofile(modpath .. "/pressure_plate.lua")
-dofile(modpath .. "/button.lua")
-dofile(modpath .. "/wrench.lua")
+modpath("/util.lua")
+modpath("/position.lua")
+modpath("/connection.lua")
+modpath("/persistance.lua")
+modpath("/power.lua")
+modpath("/wire.lua")
+modpath("/wire_node.lua")
+modpath("/lamp.lua")
+modpath("/indicator.lua")
+modpath("/inverter.lua")
+modpath("/pressure_plate.lua")
+modpath("/button.lua")
+modpath("/wrench.lua")
