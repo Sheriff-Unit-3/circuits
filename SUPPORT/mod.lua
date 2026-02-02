bba57d53fdc1188b056964dd72a1cacc3f7c46c7
Sheriff U3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-02-01 19:39:58 -0600

    removed mcl support

--- SUPPORT/mod.lua
+++ SUPPORT/mod.lua
@@ -3,8 +3,6 @@ local c = circuits
 function c.stack_max()
   if c.is_mod_enabled("default") then
     return 99
-  elseif c.is_mod_enabled("mcl_core") then
-    return 64
   elseif c.is_mod_enabled("blk") then
     return 256
   end
